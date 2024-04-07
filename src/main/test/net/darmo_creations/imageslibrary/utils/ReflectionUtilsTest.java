bf919aa9f593d18998bb93987043de7c59d1f3d4
Darmo117 <darmo117@gmail.com>
2024-04-07 18:32:21 +0200

    add tests for ReflectionUtils

---
+++ src/main/test/net/darmo_creations/imageslibrary/utils/ReflectionUtilsTest.java
@@ -0,0 +1,21 @@
+package net.darmo_creations.imageslibrary.utils;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectionUtilsTest {
+  @Test
+  void getCallingMethodName_success() {
+    assertEquals("getCallingMethodName_success", this.dummy());
+  }
+
+  private String dummy() {
+    return ReflectionUtils.getCallingMethodName();
+  }
+
+  @Test
+  void getCallingMethodName_notCaller() {
+    assertNotEquals("getCallingMethodName_notCaller", ReflectionUtils.getCallingMethodName());
+  }
+}
