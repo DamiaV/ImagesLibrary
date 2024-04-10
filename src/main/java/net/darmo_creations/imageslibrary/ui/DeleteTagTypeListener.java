346b85104e2a8ee95bb9c39bc318b529dcc7bfac
Darmo117 <darmo117@gmail.com>
2024-04-10 23:38:31 +0200

    add annotations

--- src/main/java/net/darmo_creations/imageslibrary/ui/DeleteTagTypeListener.java
+++ src/main/java/net/darmo_creations/imageslibrary/ui/DeleteTagTypeListener.java
@@ -2,6 +2,7 @@ package net.darmo_creations.imageslibrary.ui;
 
 import net.darmo_creations.imageslibrary.data.*;
 
+@FunctionalInterface
 public interface DeleteTagTypeListener {
   /**
    * Called when a tag type is to be deleted.
--- src/main/java/net/darmo_creations/imageslibrary/ui/EditTagTypeListener.java
+++ src/main/java/net/darmo_creations/imageslibrary/ui/EditTagTypeListener.java
@@ -2,6 +2,7 @@ package net.darmo_creations.imageslibrary.ui;
 
 import net.darmo_creations.imageslibrary.data.*;
 
+@FunctionalInterface
 public interface EditTagTypeListener {
   /**
    * Called when a tag type is to be edited.
