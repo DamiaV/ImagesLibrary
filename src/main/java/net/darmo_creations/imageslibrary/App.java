2862463064daaf8a908422571516a5b0e7180217
Darmo117 <darmo117@gmail.com>
2024-12-06 23:02:01 +0100

    update version to 1.1.1

--- src/main/java/net/darmo_creations/imageslibrary/App.java
+++ src/main/java/net/darmo_creations/imageslibrary/App.java
@@ -19,7 +19,7 @@ import java.util.*;
 
 public class App extends Application {
   public static final String NAME = "Image Library";
-  public static final String VERSION = "1.1";
+  public static final String VERSION = "1.1.1";
 
   private static Logger LOGGER;
 
