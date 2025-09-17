e67fa47efc78e8a5bb4689ed779c4c2a7dafc5d2
damia <damia.vergnet@protonmail.com>
2025-09-17 20:14:22 +0200

    Bump version to 1.2.1

--- src/main/java/net/darmo_creations/bildumilo/App.java
+++ src/main/java/net/darmo_creations/bildumilo/App.java
@@ -19,7 +19,7 @@ import java.util.*;
 
 public class App extends Application {
   public static final String NAME = "Bildumilo";
-  public static final String VERSION = "1.2";
+  public static final String VERSION = "1.2.1";
 
   private static Logger LOGGER;
 
